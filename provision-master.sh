42bf495437e50d153e149cb7d128982ffdc61e5b
Simon Detheridge <simon@widgit.com>
2017-10-17 14:02:19 +0100

    Ensure docker daemon listens on external port

--- provision-master.sh
+++ provision-master.sh
@@ -8,3 +8,12 @@ echo "Found swarm name: $SWARM"
 docker swarm init --advertise-addr $MASTER_IP
 docker swarm join-token worker -q > /vagrant/tmp/swarm-worker.token
 
+docker service create \
+  --name portainer \
+  --publish 9000:9000 \
+  --replicas=1 \
+  --constraint 'node.role == manager' \
+  --mount type=bind,src=//var/run/docker.sock,dst=/var/run/docker.sock \
+  portainer/portainer \
+  -H unix:///var/run/docker.sock
+
