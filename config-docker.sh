42bd4acf5269e0a481b5fbaedd54e001769953fb
Simon Detheridge <simon@widgit.com>
2018-02-20 16:33:43 +0000

    Fixes for overlapping IP ranges

--- config-docker.sh
+++ config-docker.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
+mkdir /etc/docker
 cat <<EOT | sudo tee /etc/docker/daemon.json
 {
-  "bip": "172.22.16.5/24",
-  "fixed-cidr": "172.19.55.0/24"
+  "bip": "172.22.16.5/22",
+  "fixed-cidr": "172.22.16.5/24"
 }
 EOT
 systemctl restart docker
--- provision-master.sh
+++ provision-master.sh
@@ -5,6 +5,12 @@ docker swarm join-token worker -q > /vagrant/tmp/swarm-worker.token
 
 ifconfig eth0 | grep 'inet ' | awk '{print $2}' > /vagrant/tmp/master.ip
 
+docker network create \
+  --subnet=172.22.32.5/22 \
+  -o com.docker.network.bridge.enable_icc=false \
+  -o com.docker.network.bridge.name=docker_gwbridge \
+  docker_gwbridge
+
 docker service create \
   --name portainer \
   --publish 9000:9000 \
